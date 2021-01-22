5d086a09e476c4afa84843c9c2a3ab2dde8eee32
mayf <mayf>
2021-01-22 16:40:20 +0800

    线程demo

---
+++ ThreadCreateDemo/src/main/java/cn/laomo/thread/t_003/Sync_01Reentrant.java
@@ -0,0 +1,24 @@
+package cn.laomo.thread.t_003;
+
+/**
+ * @program: ThreadDemos
+ * @description: synchronized是可重入的，相当于在同一个对象加了很多把锁；父子类同样是子类的一把锁。
+ * @author: 老马
+ * @create: 2021-01-22 15:36
+ **/
+public class Sync_01Reentrant {
+
+    private synchronized void m1() {
+        System.out.println("m1 start");
+        m2();
+        System.out.println("m1 end");
+    }
+
+    private synchronized void m2() {
+        System.out.println("m2 finished");
+    }
+
+    public static void main(String[] args) {
+        new Sync_01Reentrant().m1();
+    }
+}
---
+++ ThreadCreateDemo/src/main/java/cn/laomo/thread/t_003/Sync_02Exception.java
@@ -0,0 +1,45 @@
+package cn.laomo.thread.t_003;
+
+import lombok.SneakyThrows;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @program: ThreadDemos
+ * @description: 异常是会自动释放锁的。其他线程可以拿到有问题的数据，实际编程中小心
+ * @author: 老马
+ * @create: 2021-01-22 15:50
+ **/
+public class Sync_02Exception {
+    //没有出款的状态
+    int state = 0;
+
+    //对钱进行处理，要小心
+    @SneakyThrows
+    private synchronized void m1() {
+        String name = Thread.currentThread().getName();
+        System.out.println(Thread.currentThread().getName() + "进来进行出款操作了。");
+        if (state == 0) {
+            for (int i = 0; i < 10; i++) {
+                System.out.println("出款人"+i+"进行了出款");
+                TimeUnit.SECONDS.sleep(1);
+                if(name.equals("t1") && i == 5) {
+                    int j = i/0;
+                    System.out.println(j);
+                }
+            }
+            //标识处理完成
+            state =1;
+        }
+        System.out.println(Thread.currentThread().getName() + "出款操作成功完成了。");
+    }
+
+    @SneakyThrows
+    public static void main(String[] args) {
+        Sync_02Exception s2 = new Sync_02Exception();
+        new Thread(s2::m1, "t1").start();
+        TimeUnit.SECONDS.sleep(1);
+        new Thread(s2::m1, "t2").start();
+
+    }
+}
