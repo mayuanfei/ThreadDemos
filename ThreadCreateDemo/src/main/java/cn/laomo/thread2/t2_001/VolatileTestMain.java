50036950915fc9d350f45b2a7865d44b90e45e18
mayf <mayf>
2021-01-24 10:37:05 +0800

    线程demo

---
+++ ThreadCreateDemo/src/main/java/cn/laomo/thread2/t2_001/VolatileTestMain.java
@@ -0,0 +1,10 @@
+package cn.laomo.thread2.t2_001;
+
+/**
+ * @program: ThreadDemos
+ * @description: voldtile 保证线程可见性；有序性；但是不保证原子性
+ * @author: 老马
+ * @create: 2021-01-23 11:34
+ **/
+public class VolatileTestMain {
+}
