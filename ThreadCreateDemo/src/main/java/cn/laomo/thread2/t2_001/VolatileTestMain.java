fe6760091897430e6b3d460ac4691145b2e1a8e3
mayf <mayuanfei@163.com>
2021-01-24 11:56:34 +0800

    Thread demo

--- ThreadCreateDemo/src/main/java/cn/laomo/thread2/t2_001/VolatileTestMain.java
+++ ThreadCreateDemo/src/main/java/cn/laomo/thread2/t2_001/VolatileTestMain.java
@@ -1,5 +1,9 @@
 package cn.laomo.thread2.t2_001;
 
+import lombok.SneakyThrows;
+
+import java.util.concurrent.TimeUnit;
+
 /**
  * @program: ThreadDemos
  * @description: voldtile 保证线程可见性；有序性；但是不保证原子性
@@ -7,4 +11,23 @@ package cn.laomo.thread2.t2_001;
  * @create: 2021-01-23 11:34
  **/
 public class VolatileTestMain {
+    public volatile boolean running = true;
+
+    @SneakyThrows
+    void m1() {
+        System.out.println("m1 start");
+        while(running) {
+            //TimeUnit.SECONDS.sleep(1);
+            //System.out.println("aaa");
+        }
+        System.out.println("m1 end");
+    }
+
+    @SneakyThrows
+    public static void main(String[] args) {
+        VolatileTestMain test = new VolatileTestMain();
+        new Thread(test::m1).start();
+        TimeUnit.SECONDS.sleep(1);
+        test.running = false;
+    }
 }
