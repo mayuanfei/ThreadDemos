9d65a3cf1278836cdcf56187fc41c1ba94fb239a
mayf <mayf>
2021-01-28 17:33:55 +0800

    线程demo

---
+++ ThreadCreateDemo/src/main/java/cn/laoma/thread2/t2_003/Lock_13LockSupport.java
@@ -0,0 +1,32 @@
+package cn.laoma.thread2.t2_003;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.LockSupport;
+
+/**
+ * @program: ThreadDemos
+ * @description: 交换器
+ * 作用：阻塞线程用的
+ * @author: 老马
+ * @create: 2021-01-28 14:51
+ **/
+public class Lock_13LockSupport {
+    public static void main(String[] args) {
+        Thread t1 = new Thread(() -> {
+            for (int i = 0; i < 10; i++) {
+                System.out.println("index = " + i);
+                if(i == 3) {
+                    LockSupport.park();
+                }
+                try {TimeUnit.SECONDS.sleep(1);} catch (InterruptedException e) {}
+            }
+        });
+        t1.start();
+        //unpark在park之前执行
+//        LockSupport.unpark(t1);
+        //unpark在park之后执行
+        try {TimeUnit.SECONDS.sleep(7);} catch (InterruptedException e) {}
+        System.out.println("释放t1的锁");
+        LockSupport.unpark(t1);
+    }
+}
